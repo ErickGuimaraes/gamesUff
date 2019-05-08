9d37c95505c70cac7bb807ecad90e8aa7a40fa35
Erick Guimaraes <erickgdeoliveira@gmail.com>
2019-05-08 17:44:36 -0300

    MAking first scene

---
+++ Assets/Scripts/prfEx2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prfEx2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Eu n'ao sou um exemplo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
