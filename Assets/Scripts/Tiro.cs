5a24e850915f3713d887defa62d0dcf0db2025fb
unknown <machado_gomes@hotmail.com>
2019-06-05 16:32:50 -0300

    talquei

--- Assets/Scripts/Tiro.cs
+++ Assets/Scripts/Tiro.cs
@@ -6,6 +6,14 @@ public class Tiro : MonoBehaviour
 {
     public float velocidade;
     void Update(){
+        moveTiro();
+    }
+
+    void moveTiro(){
         transform.Translate(0, (-velocidade * Time.deltaTime), 0);
+    }
+
+    void onTriggerEnter(Collider other){
+        
     }
 }
