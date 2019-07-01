a0743004f7c2d70ea45d45ae6084a41e149dff1a
unknown <machado_gomes@hotmail.com>
2019-07-01 15:39:52 -0300

    spawning enemies on level 1

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,28 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject enemy;
+    float randX,randY;
+    Vector3 spawnPlace;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Time.time > nextSpawn){
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-8.4f, 8.4f);
+            randY = Random.Range(-8.4f, 8.4f);
+            spawnPlace = new Vector3(randX, randY, transform.position.z);
+            Instantiate(enemy, spawnPlace, enemy.transform.rotation);
+        }
+    }
+}
